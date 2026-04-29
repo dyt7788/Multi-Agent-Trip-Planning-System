198a4a94bd1be577a4998f5f70b2fd014693305f
dyt7788 <73678248+dyt7788@users.noreply.github.com>
2026-04-29 19:59:37 +0800

    Delete backend directory

--- backend/app/aipy/__init__.py
+++
@@ -1,9 +0,0 @@
-"""AiPy-style code generation and execution agents."""
-
-from .travel_codegen_agent import TravelCodegenAgent, AgentRunResult, AgentStep
-
-__all__ = [
-    "TravelCodegenAgent",
-    "AgentRunResult",
-    "AgentStep",
-]
